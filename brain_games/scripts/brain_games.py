2f1844e9aa3556020a4d50828c3079ebe0a81ba1
Zhuravlev Danila <daniljudo2@gmail.com>
2026-04-16 21:37:50 +0700

    update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,9 @@
-from brain_games.engine.push import greet
+from brain_games.engine.push import greet, welcome_user
 
 
 def main():
     greet()
+    welcome_user()
    
 
 if __name__ == "__main__":
